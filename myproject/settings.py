91001981fd9cb8ff962ea035ef5a07be399e9342
Sagar Singh <sagars9417@gmail.com>
2026-07-31 11:08:38 +0100

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
